df0019aace6eabb82875de382895c581b9601021
sc21ek2@leeds.ac.uk <sc21ek2@leeds.ac.uk>
2025-03-31 13:17:56 +0100

    added comments

--- ros2_project_sc21ek2/final_project.py
+++ ros2_project_sc21ek2/final_project.py
@@ -1,58 +1,38 @@
-# COMMANDS
-# 
-# ~/ros2_ws $ ros2 launch turtlebot3_navigation2 navigation2.ros2 run ros2_project_sc21ek2 final_project
-# launch.py use_sim_time:=True map:=$HOME/ros2_ws/src/ros2_project_sc21ek2/map/map.yaml
-# ros2 launch turtlebot3_gazebo turtlebot3_task_world_2025.launch.py
-# source ~/.bashrc
-
-
-# Exercise 1 - Display an image of the camera feed to the screen
 import rclpy
 from rclpy.node import Node
 from rclpy.action import ActionClient
-from math import sin, cos
 import cv2
-import numpy as np
 from cv_bridge import CvBridge
-from geometry_msgs.msg import Twist, PoseStamped
 from sensor_msgs.msg import Image
-import signal
 from nav2_msgs.action import NavigateToPose
-import random
+from math import sin, cos
+import numpy as np
 import time
-
 
 class RGBNavigator(Node):
     def __init__(self):
         super().__init__('rgb_nav')
         
-        self.sensitivity = 10
+        self.sensitivity = 10 #hsv colour filtering
         self.goal_active = False
-
+        
+        #navigate action client and camera
         self.nav_client = ActionClient(self, NavigateToPose,'navigate_to_pose')
         self.subscription = self.create_subscription(Image, '/camera/image_raw', self.callback, 30)
         self.bridge = CvBridge()
-                
-        self.found_red = False;
-        self.found_green = False;
-        self.found_blue = False;
-        self.reach_blue = False;        
-        
+    
+        #List of coordinates        
         self.target_coords = [
-            (-3.9, -1.65, 0.00247),
-            (3.07,-7.35,-0.00143),
-            (-3.08,-9.16,-0.00143),
-           # (2.16, -8.04, -0.00134),
-           # (1.99,-9.21,-0.00143),
-           # (-2.75,-9.11,-0.00143),
+            (-3.9, -1.65, 3.14),
+            (3.07,-7.35,0.0),
+            (-3.08,-9.16,3.14),
             
         ]
         self.index = 0
-        
-        self.timer = self.create_timer(2.0, self.timer_callback)
+        self.timer = self.create_timer(1.0,self.timer_callback) #trigger goal send
     
     
-    def send_goal(self,x,y,yaw):
+    def send_goal(self,x,y,yaw): #sends goasl to nav2
             goal_msg = NavigateToPose.Goal()
             goal_msg.pose.pose.position.x = x
             goal_msg.pose.pose.position.y = y
@@ -66,39 +46,28 @@ class RGBNavigator(Node):
 
             self.send_goal_future.add_done_callback(self.goal_response_callback)
             
-    def timer_callback(self):
+    def timer_callback(self):  #timer to manage goal progression
         if self.goal_active:
             return
-        if not all ([self.found_red, self.found_green, self.found_blue]):
-            if self.index <len(self.target_coords):
-                x,y, yaw = self.target_coords[self.index]
-                self.send_goal(x,y,yaw)
-                self.get_logger().info('Goal sent')
-                self.index+=1
-                self.goal_active= True
-            else:
-                self.get_logger().info('not all colours Detected ')
-
-        elif self.found_blue and not self.reach_blue:
-                self.get_logger().info('moving towards blue box')
-                x,y, yaw = self.target_coords[2]
-                self.send_goal(x,y,yaw)
-                
-    def goal_response_callback(self, future):
+        if self.index <len(self.target_coords):
+            x,y, yaw = self.target_coords[self.index]
+            self.send_goal(x,y,yaw)
+            self.goal_active= True
+                 
+    def goal_response_callback(self, future):  #cllback when goal is rejected
         goal_handle = future.result()
         if not goal_handle.accepted:
             self.get_logger().info('Goal rejected')
             return
 
-        self.get_logger().info('Goal accepted')
+        self.get_logger().info('Goal accepted') #cllback when goal is accepted
         self.get_result_future = goal_handle.get_result_async()
         self.get_result_future.add_done_callback(self.get_result_callback)
 
     def get_result_callback(self, future):
         result = future.result().result
         self.get_logger().info(f'Navigation result: {result}')
-        self.goal_active = False;
-
+        self.goal_active = False
 
     def feedback_callback(self, feedback_msg):
             feedback = feedback_msg.feedback
@@ -106,15 +75,11 @@ class RGBNavigator(Node):
     def callback(self, data):
             image = self.bridge.imgmsg_to_cv2(data, 'bgr8')
             Hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-
-
-            
+        
             colours_hsv = {
                 'red_image': cv2.inRange(Hsv_image, np.array([0 - self.sensitivity, 100, 100]), np.array([0 + self.sensitivity, 255, 255])),
                 'green_image': cv2.inRange(Hsv_image, np.array([60 - self.sensitivity, 100, 100]), np.array([60 + self.sensitivity, 255, 255])),
-                'blue_image': cv2.inRange(Hsv_image, np.array([110 - self.sensitivity, 100, 50]), np.array([130 + self.sensitivity, 255, 255])),
-
-
+                'blue_image': cv2.inRange(Hsv_image, np.array([110 - self.sensitivity, 100, 50]), np.array([130 + self.sensitivity, 255, 255]))
 
             }
             
@@ -129,20 +94,6 @@ class RGBNavigator(Node):
                                 center = (int(x),int(y)) 
                                 radius = int(radius) 
                                 cv2.circle(image,center,radius,(255,255,0) ,1)
-                                
-                                if colour == 'red' and not self.found_red:
-                                    self.found_red = True
-                                    self.get_logger().info('red detected')
-
-                                
-                                if colour == 'green' and not self.found_green:
-                                    self.found_green = True
-                                    self.get_logger().info('green detected')
-
-                                    
-                                if colour == 'blue' and not self.found_blue:
-                                    self.found_blue = True
-                                    self.get_logger().info('blue detected')
     
             cv2.namedWindow('camera_Feed',cv2.WINDOW_NORMAL)
             cv2.imshow('camera_Feed', image)
@@ -162,7 +113,6 @@ def main():
             node.destroy_node()
             rclpy.shutdown()
             cv2.destroyAllWindows()
-# Check if the node is executing in the main path
 if __name__ == '__main__':
     main()
 
