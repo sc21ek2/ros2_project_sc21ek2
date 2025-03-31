9f37dc993506637857b7efda322e5642f31d8151
sc21ek2@leeds.ac.uk <sc21ek2@leeds.ac.uk>
2025-03-31 10:10:45 +0100

    changes 31/mar

--- ros2_project_sc21ek2/final_project.py
+++ ros2_project_sc21ek2/final_project.py
@@ -9,7 +9,6 @@
 # Exercise 1 - Display an image of the camera feed to the screen
 import rclpy
 from rclpy.node import Node
-from rclpy.exceptions import ROSInterruptException
 from rclpy.action import ActionClient
 from math import sin, cos
 
@@ -23,137 +22,138 @@ import signal
 from nav2_msgs.action import NavigateToPose
 import random
 
-class colourIdentifier(Node):
+class RGBNavigator(Node):
     def __init__(self):
-        super().__init__('cI')
-        # Remember to initialise a CvBridge() and set up a subscriber to the image topic you wish to use
-        # We covered which topic to subscribe to should you wish to receive image data
+        super().__init__('rgb_nav')
+        
+        self.sensitivity = 10
 
-        self.bridge = CvBridge()
-        self.subscription = self.create_subscription(Image, '/camera/image_raw', self.image_callback, 30)
-        self.subscription  # prevent unused variable warning
-        self.publisher = self.create_publisher(Twist, '/cmd_vel', 10)
-        self.move_cmd = Twist()
-        self.target_detected = False
-        
         self.nav_client = ActionClient(self, NavigateToPose,'navigate_to_pose')
-        self.goal_running = False
-        self.timer = self.create_timer(10.0, self.send_random_goal)
+        self.subscription = self.create_subscription(Image, '/camera/image_raw', self.callback, 30)
+        self.bridge = CvBridge()
         
-    def send_random_goal(self):
-        if self.target_detected or self.goal_running:
-            return
-   
+        self.found_red = False;
+        self.found_green = False;
+        self.found_blue = False;
+        self.reach_blue = False;
         
-        goal = NavigateToPose.Goal()
-        goal.pose.pose.position.x = 0.313
-        goal.pose.pose.position.y = -7.81
-        goal.pose.pose.orientation.z = sin(0.00256 / 2)
-        goal.pose.header.frame_id = 'map'        
-        self.goal_running=True
-        self.nav_client.send_goal_async(goal).add_done_callback(self.get_result_callback)
+        self.target_coords = [
+            (-2.18, -4.17, 0.00256),
+            (1.64,-7.46,0.00641),
+            (0.119,-10.8,0.00641)
+        ]
         
-        self.get_logger().info("sending goal")
+        self.index = 0
         
-    def goal_response_callback(self,future):
+        self.timer = self.create_timer(2.0, self.timer_callback)
+    
+    
+    def send_goal(self,x,y,yaw):
+            goal_msg = NavigateToPose.Goal()
+            goal_msg.pose.pose.position.x = x
+            goal_msg.pose.pose.position.y = y
+            goal_msg.pose.pose.orientation.z = sin(yaw / 2)
+            goal_msg.pose.pose.orientation.w = cos(yaw / 2)
+            goal_msg.pose.header.frame_id = 'map'
+            goal_msg.pose.header.stamp = self.get_clock().now().to_msg()
+
+            self.nav_client.wait_for_server()
+            self.send_goal_future = self.nav_client.send_goal_async(goal_msg, feedback_callback=self.feedback_callback)
+
+            self.send_goal_future.add_done_callback(self.goal_response_callback)
+            
+    def timer_callback(self):
+        if not all ([self.found_red, self.found_green, self.found_blue]):
+            if self.index <len(self.target_coords):
+                x,y, yaw = self.target_coords[self.index]
+                self.send_goal(x,y,yaw)
+                self.get_logger().info('Goal sent')
+                self.index+=1
+            else:
+                self.get_logger().info('not all colours Detected ')
+
+        elif self.found_blue and not self.reach_blue:
+                self.get_logger().info('moving towards blue box')
+                x,y, yaw = self.target_coords[2]
+                self.send_goal(x,y,yaw)
+                
+    def goal_response_callback(self, future):
         goal_handle = future.result()
         if not goal_handle.accepted:
             self.get_logger().info('Goal rejected')
             return
 
         self.get_logger().info('Goal accepted')
-        
-        goal_handle.get_result_async().add_done_callback(self.get_result_callback)
-       
+        self.get_result_future = goal_handle.get_result_async()
+        self.get_result_future.add_done_callback(self.get_result_callback)
+
     def get_result_callback(self, future):
-        try:
-            result = future.result()
-            self.get_logger().info(f'Navigation result: {result}') 
-        except Exception as e:
-                self.get_logger().error(f'{e}') 
+        result = future.result().result
+        self.get_logger().info(f'Navigation result: {result}')
 
-        self.goal_running = False
 
-    def image_callback(self, data):
-        # Convert the received image into a opencv image
-        # But remember that you should always wrap a call to this conversion method in an exception handler
-        # Show the resultant images you have created.
+    def feedback_callback(self, feedback_msg):
+            feedback = feedback_msg.feedback
+            print(feedback)           
 
-        image = self.bridge.imgmsg_to_cv2(data, 'bgr8')
+        
+    def callback(self, data):
+            image = self.bridge.imgmsg_to_cv2(data, 'bgr8')
+            Hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+
+            cv2.namedWindow('camera_Feed',cv2.WINDOW_NORMAL)
+            cv2.imshow('camera_Feed', image)
+            cv2.resizeWindow('camera_Feed',320,240)
+            cv2.waitKey(3)
+            
+            colours_hsv = {
+                'red_image': cv2.inRange(Hsv_image, np.array([0 - self.sensitivity, 100, 100]), np.array([0 + self.sensitivity, 255, 255])),
+                'green_image': cv2.inRange(Hsv_image, np.array([60 - self.sensitivity, 100, 100]), np.array([60 + self.sensitivity, 255, 255])),
+                'blue_image': cv2.inRange(Hsv_image, np.array([100 - self.sensitivity, 150, 50]), np.array([100 + self.sensitivity, 255, 255])),
+
+
+
+            }
+            
+            for colour, colourhsv in colours_hsv.items():
+                contours,_ = cv2.findContours(colourhsv,mode = cv2.RETR_TREE, method = cv2.CHAIN_APPROX_SIMPLE )
+                for c in contours:
+                    if cv2.contourArea(c) > 100: 
+                            M = cv2.moments(c)
+                            if M['m00'] != 0:
+                                cx, cy = int(M['m10']/M['m00']), int(M['m01']/M['m00'])
+                                (x, y), radius = cv2.minEnclosingCircle(c)
+                                center = (int(x),int(y)) 
+                                radius = int(radius) 
+                                cv2.circle(image,center,radius,(255,255,0) ,1)
+                                
+                                if colour == 'red' and not self.found_red:
+                                    self.found_red = True
+                                    self.get_logger().info('red detected')
+
+                                
+                                if colour == 'green' and not self.found_green:
+                                    self.found_green = True
+                                    self.get_logger().info('green detected')
+
+                                    
+                                if colour == 'blue' and not self.found_blue:
+                                    self.found_blue = True
+                                    self.get_logger().info('blue detected')
     
-        Hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-        hsv_blue_lower = np.array([120- 10, 100, 100])
-        hsv_blue_upper = np.array([120+ 10, 255, 255])
-        
-        blue_image = cv2.inRange(Hsv_image, hsv_blue_lower, hsv_blue_upper)
+            cv2.imshow('Colour DEtected', image)
+            cv2.waitKey(3)
 
-        contours, _ = cv2.findContours(blue_image,mode = cv2.RETR_TREE, method = cv2.CHAIN_APPROX_SIMPLE )
-        if len(contours) > 0:
-            
-            # Loop over the contours
-            # There are a few different methods for identifying which contour is the biggest:
-            # Loop through the list and keep track of which contour is biggest or
-            # Use the max() method to find the largest contour
-            c = max(contours, key=cv2.contourArea)
-            #Check if the area of the shape you want is big enough to be considered
-            # If it is then change the flag for that colour to be True(1)
-            if cv2.contourArea(c) > 100: #<What do you think is a suitable area?>
-                            #Moments can calculate the center of the contour
-                M = cv2.moments(c)
-                
-                if M['m00'] == 0:
-                    return
-                
-                cx = int(M['m10']/M['m00'])
-                width = image.shape[1]
-                error = cx - width // 2
-                # draw a circle on the contour you're identifying
-                
-                if cv2.contourArea(c)>1000:
-                    self.move_cmd.linear.x =0.0
-                    self.move_cmd.angular.z =0.0
-                    self.target_detected = True
-                    self.get_logger().info("BLUE BOX WITHIN ONE METER, STOPPING")
-                    
-                else:
-                    self.move_cmd.linear.x =0.15
-                    self.move_cmd.angular.z = -float(error)/200       
-                    self.get_logger().info("APPROACHING BLUE BOX")
-            
-        else:
-            self.move_cmd.linear.x =0.0
-            self.move_cmd.angular.z = 0.2      
-        
-        self.publisher.publish(self.move_cmd)
-        
-        # filtered = cv2.bitwise_and(image,image,mask=blue_image)
-        # cv2.namedWindow("camera_Feed",cv2.WINDOW_NORMAL)
-        # cv2.resizeWindow('camera_Feed',320,240)
-        # cv2.waitKey(3)
- 
-        # cv2.imshow('camera_Feed', filtered)
-       #  cv2.waitKey(3)
-        
-        
-        
+
 def main():
 
     rclpy.init(args=None)
-    # Ensure that the node continues running with rospy.spin()
-    # You may need to wrap rospy.spin() in an exception handler in case of KeyboardInterrupts
-    # Remember to destroy all image windows before closing node
-    
-
+    node = RGBNavigator()
     try:
-        node = colourIdentifier()
-        rclpy.spin(node)
-            
-    except Exception as e:
-        print (f"exception: {e}")
+        rclpy.spin(node)        
+    except KeyboardInterrupt:
         pass
-    
     finally:
-        if node is not None:
             node.destroy_node()
             rclpy.shutdown()
             cv2.destroyAllWindows()
